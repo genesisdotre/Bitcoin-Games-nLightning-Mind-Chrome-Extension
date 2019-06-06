834b92ff43582b6484bc31859cdf485d90014886
M <>
2019-06-07 00:27:21 +0200

    Moving satoshis like crazy!

--- iframed.js
+++ iframed.js
@@ -1,8 +1,7 @@
 console.log("I'm included in iframed.html, my name is iframed.js");
 
 let intervalId;
-let timeSeconds;
-const MINUTE = 5; // not 60 as non-DEBUG minute
+let timeSeconds = 500;
 
 function timeIsUp() {
     window.top.postMessage('timeIsUp', '*');
@@ -19,24 +18,9 @@ $("#closeTheTab").on("click", function() {
     _closeTab();
 });
 
-$("#addMinute").on("click", function() {
-    timeSeconds = MINUTE;
-    startCountdown();
-});
-
-function formatTime(input) {
-    let minutes = Math.floor(input / 60);
-    let seconds = input % 60;
-    function pad(num, size){ return ('000000000' + num).substr(-size); } // https://stackoverflow.com/a/2998822/775359
-    return pad(minutes, 2) + ":" + pad(seconds, 2);
-}
-
 $("#form").on("submit", function(event) {
     let task = $("#task").val();
-    $("#workingOn").text(task); // copy past from the form into another screen
-
-    timeSeconds = parseInt( $("#time-number").val() ) * MINUTE;
-    console.log("Task to accomplish: " + task + " in: " + timeSeconds + " seconds");
+    $("#workingOn").text(task); // copy paste from the form
 
     startCountdown();
 
@@ -44,14 +28,6 @@ $("#form").on("submit", function(event) {
     return false;
 });
 
-let initialValue = 10000; $("#time-range").val(initialValue); $("#time-number").val(initialValue);
-
-$("#time-range").on("input", function() {
-    $("#time-number").val( $(this).val() )
-})
-$("#time-number").on("input", function() {
-    $("#time-range").val( $(this).val() )
-})
 
 function startCountdown() {
     $(".screen").hide();
@@ -61,7 +37,6 @@ function startCountdown() {
         if (timeSeconds === 0) {
             timeIsUp();
         }
-        $("#timeLeft").text(formatTime(timeSeconds));
         timeSeconds--;
     }
     countdown(); // calling it once immediately and then again in the setInterval
@@ -77,10 +52,84 @@ function _closeTab() {
 }
 
 
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    console.log(tabs);
-    var tab = tabs[0];
-    var url = new URL(tab.url)
-    var domain = url.hostname
-    $("#thisSite").text(domain);
-})
+// chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//     console.log(tabs);
+//     var tab = tabs[0];
+//     var url = new URL(tab.url)
+//     var domain = url.hostname
+//     $("#thisSite").text(domain);
+// })
+
+
+// ANIMATED FLOW OF SATOSHIS
+
+let start = new Date();
+
+function getVariation() {
+  let rnd = Math.floor(Math.random() * 1000);
+  let variation = 0;
+
+  if (rnd < 100) {
+    variation = -3;
+  } else if (rnd < 250) {
+    variation = -2;
+  } else if (rnd < 400) {
+    variation = -1;
+  } else if (rnd < 600) {
+    variation = 0;
+  } else if (rnd < 750) {
+    variation = 1;
+  } else if (rnd < 900) {
+    variation = 2;
+  } else if (rnd < 1000) {
+    variation = 3;
+  }
+
+  return variation;
+}
+
+function moveMe() {
+  let now = new Date();
+  let diff = new Date(now - start);
+
+  let milliseconds = (diff.getMinutes() * 60 + diff.getSeconds()) * 1000 + diff.getMilliseconds()
+  let satoshisPerMillisecond = 347.22 / 1000; // just like example https://bitcoin.stackexchange.com/q/88117/11112
+
+  let satoshisText = "฿ 0." +("00000000" + Math.round(milliseconds * satoshisPerMillisecond)).slice(-8)
+  $("#satoshis").text(satoshisText);
+
+  // MOVE THE THINGS...
+  $("img.sat").toArray().forEach(function(sat) {
+
+    // LEFT LEFT LEFT LEFT
+    let currentLeft = parseFloat( $(sat).css("left").replace("px", "") );
+    let variation = getVariation();
+    let newLeft = currentLeft + 5 + variation;
+
+    if (newLeft > 430) { // far behind screen
+      $(sat).css({display: "none"}); // need to disable transition otherwise looks crap
+
+      setTimeout(function() {
+        $(sat).css({left: (-32 + getVariation())+"px", top: "10px", display: "block"});
+      }, 300)
+      
+      return;
+    } else {
+      $(sat).css({"left": newLeft+"px"});
+    }
+
+    // TOP TOP TOP TOP TOP
+    let currentTop = parseFloat( $(sat).css("top").replace("px", "") );
+
+    variation = getVariation();
+
+    let newTop = currentTop + variation;
+    newTop = Math.min(newTop, 20);
+    newTop = Math.max(newTop, 0);
+    $(sat).css({"top": newTop});
+  });
+
+  requestAnimationFrame(moveMe);
+}
+
+requestAnimationFrame(moveMe);
