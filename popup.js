d13330faa6dbd6bf7ceedfff3a3053ea02cd5faa
M <>
2019-06-07 08:17:57 +0200

    Cleanup

--- popup.js
+++ popup.js
@@ -4,9 +4,31 @@ let websites = [];
 chrome.storage.sync.get('websites', function(data) {
   if(data && data.websites) {
     websites = data.websites;
+  } else {
+    websites = defaultWebsites; // initially when there is nothing there
   }
-  websitesMarkup();
 });
+
+chrome.storage.sync.get('satoshis', function(data) {
+  if(data && data.satoshis) {
+    satoshisPerSecond = data.websites;
+  } else {
+    satoshisPerSecond = 3;
+  }
+});
+
+$.get("https://api.coindesk.com/v1/bpi/currentprice.json", function(response) {
+  let BTCUSD = JSON.parse(response).bpi.USD.rate_float;
+  price1sat = BTCUSD / 100000000;
+
+  // Satoshis are limited between 1 and 1000. USD has max slider value depending on price.
+  let maxPerHour = Math.ceil(1000 * price1sat * 3600);
+  $("#dol-number").attr("max", maxPerHour);
+  $("#dol-range").attr("max", maxPerHour);
+})
+
+
+
 
 function websitesMarkup() {
   if (websites.length > 0) {
@@ -69,126 +91,20 @@ $("#websites").on("click", ".x", function() {
   websitesMarkup();
 })
 
-$("#macaroon").on("change", function(event) {
-  file = event.target.files[0]; 
-  if (file){
-    fileReader = new FileReader();
-    fileReader.onload = function(e) {
-      xxx = buffer.Buffer.from(e.target.result).toString("hex");
-      console.log(xxx);
-
-
-    };
-    fileReader.readAsBinaryString(file);
-  }
-});
-
-
-let macaroon = "0201036c6e6402cf01030a1054a03ffe820ed30ff7301b5b7645e6a21201301a160a0761646472657373120472656164120577726974651a130a04696e666f120472656164120577726974651a170a08696e766f69636573120472656164120577726974651a160a076d657373616765120472656164120577726974651a170a086f6666636861696e120472656164120577726974651a160a076f6e636861696e120472656164120577726974651a140a057065657273120472656164120577726974651a120a067369676e6572120867656e6572617465000006206fbbce082e4ed2359d5eb9cbf87d2365d4388394a4022db180c51e8eac2d3a5d";
-
-
-$("#pay").on("click", function() {
-  let lnInvoice = $("#invoice").val();
-
-  var requestBody = { 
-    payment_request: lnInvoice,
-  };
-  
-  $.ajax({
-      url: 'https://localhost:8081/v1/channels/transactions',
-      type: 'post',
-      data: JSON.stringify(requestBody),
-      headers: {
-        'grpc-metadata-macaroon': macaroon
-      },
-      dataType: 'json',
-      success: function (data) {
-          console.info(data);
-      }
-  });
-
-});
-
-$("#getinfo").on("click", function() {
-
-  $.ajax({
-    url: 'https://127.0.0.1:8081/v1/payreq/lnbc120n1pw0sdp8pp5446htvu398ke03nvtw3rzh50l4n50dzzpcv3ck4mefp9ry3fg67sdqqcqzpgyuldnperv4jm7egv5vs3r3h00jutc4cw8duu5m5cqdlwg6yaz07nphartralf9zg6ehmzmhgnrktrnegqzcyj0zdrdgfzaxm76aktucpqq3t0t',
-    type: 'get',
-    headers: {
-      'grpc-metadata-macaroon': macaroon
-    },
-    dataType: 'json',
-    success: function (data) {
-        console.info(data);
-    }
-  });
-  
-})
-
-$("#pay2").on("click", function() {
-  let lnInvoice = $("#invoice").val();
-  payInvoice(lnInvoice);
-});
-
-
-function payInvoice(invoice) {
-  console.log("Received invoice, will pay: " + invoice);
-  return new Promise(function(resolve, reject) {
-
-        var requestBody = { 
-          payment_request: invoice,
-        };
-       
-        $.ajax({
-            url: 'https://localhost:8081/v1/channels/transactions',
-            type: 'post',
-            form: JSON.stringify(requestBody),
-            headers: {
-              'Grpc-Metadata-macaroon': macaroon
-            },
-            dataType: 'json',
-            success: function (data) {
-                console.info(data.lightning_invoice.payreq);
-            }
-        });
-
-  });
-}
 
 
 
 
-
-
-
-
-
-// as a user I can slide `sat`
-// as a user I can slide `usd`
-// all 4 input fields need to stay in sync
-let satoshisPerSecond = 100; // this is the base currency
+// SLIDERS SLIDERS SLIDERS SLIDERS
+let satoshisPerSecond;
 let dollarsPerHour;
+let price1sat;
 $("#sat-range").val(satoshisPerSecond);
 $("#sat-number").val(satoshisPerSecond);
 
-$.get("https://api.coindesk.com/v1/bpi/currentprice.json", function(response) {
-  let BTCUSD = JSON.parse(response).bpi.USD.rate_float;
-  price1sat = BTCUSD / 100000000;
-  updateSliders();
-  setMaxDollarValues();
-})
-
-
-// Satoshis are limited between 1 and 1000. For USD it will be different. Always `sat` as base currency
-function setMaxDollarValues() {
-  let maxPerHour = Math.ceil(1000 * price1sat * 3600);
-  $("#dol-number").attr("max", maxPerHour);
-  $("#dol-range").attr("max", maxPerHour);
-}
-
 function updateSliders(satoshis) {
   if (satoshis === false) { // option when it was dollars per hour that changed, otherwise default satoshis per second
-    satoshisPerSecond = Math.floor((dollarsPerHour / 3600) / price1sat); // don't want to get 1001
+    satoshisPerSecond = Math.min(1000, Math.floor((dollarsPerHour / 3600) / price1sat)); // don't want to have 1001 or 1002 it looks weird
     $("#sat-number").val(satoshisPerSecond);
     $("#sat-range").val(satoshisPerSecond);
   } else {
@@ -196,6 +112,10 @@ function updateSliders(satoshis) {
     $("#dol-number").val(dollarsPerHour);
     $("#dol-range").val(dollarsPerHour);
   }
+
+  return chrome.storage.sync.set({ satoshis: satoshisPerSecond }, function() {
+    console.log("storage updated, satoshisPerSecond: " + satoshisPerSecond);
+  });  
 }
 
 $("#sat-range").on("input", function() {
@@ -219,6 +139,11 @@ $("#dol-number").on("input", function() {
   updateSliders(false);
 })
 
+
+
+
+
+
 // NAVIGATION
 $(".nav").on("click", ".tab", function() {
 
@@ -228,6 +153,22 @@ $(".nav").on("click", ".tab", function() {
 
 
 
+
+
+// MACAROON BUFFER TO HEX
+$("#macaroon").on("change", function(event) {
+  file = event.target.files[0]; 
+  if (file){
+    fileReader = new FileReader();
+    fileReader.onload = function(e) {
+      xxx = buffer.Buffer.from(e.target.result).toString("hex");
+      console.log(xxx);
+
+
+    };
+    fileReader.readAsBinaryString(file);
+  }
+});
 
 
 
